7fefcc0ad77b2284f70cdb09a7659ec957562f69
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-01-08 22:07:30 +0100

    add: view template

---
+++ views/viewTemplate.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// TODO: import your components and containers 
+// you want to use in this view
+
+require('../../scss/style.scss');
+
+
+// TODO: add your imported components and containers
+const About = () => (
+
+  <div>
+    VIEW TEMPLATE
+  </div>
+
+);
+
+
+export default About;
