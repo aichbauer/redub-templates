887f354dd05a44bd3c5ef65478401f0ef9420686
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-01-08 21:56:20 +0100

    add: template for components

---
+++ components/componentTemplate.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+
+// TODO: change ComponentTemplate
+// eg. Navigation
+class ComponentTemplate extends Component {
+
+  render() {
+
+    // TODO: Add your Component
+    return (
+
+      <div>
+      </div>
+
+    );
+
+  }
+
+}
+
+
+/////////////
+// WARNING //
+/////////////
+
+// rarely use this on components 
+// if your component uses actions
+// its pobably better as a container
+function matchDispatchToProps(dispatch) {
+
+  return bindActionCreators({}, dispatch);
+
+}
+
+/////////////
+// WARNING //
+/////////////
+
+// rarely use this on components 
+// if your component depends on too much states
+// its pobably better as a container
+function mapStateToProps(state) {
+
+  return {}
+
+}
+
+
+// TODO: change ComponentTemplate to the name of your Component
+export default connect(mapStateToProps, matchDispatchToProps)(ComponentTemplate);
