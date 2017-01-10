a32a2def0c840c847d7f60902e3fdc13868214dd
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-01-10 15:05:13 +0100

    update: path to style

--- views/template/viewTemplate.js
+++ views/template/viewTemplate.js
@@ -3,7 +3,7 @@ import React from 'react';
 // TODO: import your components and containers 
 // you want to use in this view
 
-require('../../scss/style.scss');
+require('../../../scss/style.scss');
 
 
 // TODO: add your imported components and containers
